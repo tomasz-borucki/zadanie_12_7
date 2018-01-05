0512abfc37b1aabfc1aaa59e6e864e5ea35b067a
Your Name <you@example.com>
2018-01-05 12:11:21 +0100

    add files to project

---
+++ App.js
@@ -0,0 +1,33 @@
+var baseUrl = 'https://kodilla.com/pl/bootcamp-api/';
+var myHeaders = {
+  'X-Client-Id': "2414",
+  'X-Auth-Token': "5befc4ffadb3cbf50eebd2cdcf4fbc3a"
+};
+
+$.ajaxSetup({
+	headers: myHeaders
+});
+
+$.ajax({
+    url: baseUrl + 'board',
+    method: 'GET',
+    success: function(response) {
+      setupColumns(response.columns);
+    }
+});
+
+function setupColumns(columns) {
+    columns.forEach(function (column) {
+  		var col = new Column(column.id, column.name);
+        board.createColumn(col);
+        setupCards(col, column.cards);
+    });
+};
+
+function setupCards(col, cards) {
+	cards.forEach(function (card) {
+        var card = new Card(card.id, card.name, card.bootcamp_kanban_column_id);
+    	col.createCard(card);
+  	})
+};
+
